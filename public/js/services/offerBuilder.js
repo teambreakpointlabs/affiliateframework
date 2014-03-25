7dc3e85a6f33258fd4c74f5aa3e9daac4baada73
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-03-25 13:53:43 +0000

    handled undefined original offer price

--- public/js/services/offerBuilder.js
+++ public/js/services/offerBuilder.js
@@ -12,22 +12,27 @@ angular.module('mean.system').service('OfferBuilderService', function(){
 	}
 
 	var buildIndividualOffer = function(offer){
+
+    var originalPriceDisplay = '';
+
+    if (offer.pricing.original != undefined){
+      originalPriceDisplay = '<div class="indiv_offer_label indiv_original_price"> Was £' + offer.pricing.original+ '</div>';
+    }
+    
+
 		return  '<div class="indiv_offer_brand">' + offer.brand + ' ' + offer.type + ' Offer </div>' + 
     '<div class="indiv_offer_description">' + offer.description +'</div>' + '<div class="indiv_offer_valid"> This offer is <span class="indiv_offer_valid_text">valid</span></div>' +
     '<div class="indiv_offer_img_holder"><img class="indiv_offer_img" src="'+ offer.url.image +'"/></div>'+'<div>'+
     '<div class="indiv_offer_label indiv_go_to_offer"><a class="indiv_go_to_offer_text" ng-href='+offer.url.skimlinks+'> Go to Offer <span class="glyphicon glyphicon-chevron-right" style="color:#38d5b8;font-size:14px;"></span></a></div>' +
     '<div class="indiv_offer_label indiv_offer_price"> £' + offer.pricing.offer+ '</div>' +
-    '<div class="indiv_offer_label indiv_original_price"> Was £' + offer.pricing.original+ '</div>' +
+    originalPriceDisplay +
     '<div class="indiv_offer_label indiv_saving"> Save ' + offer.pricing.pctSavings+ '%!</div>' +
     '<div class="indiv_logo_holder"><img ng-src="/img/logos/'+offer.retailer+'.png"/></div>';
   }
 
 	var buildListOffer = function(offer){
     
-  
-
     var standardTagline = '<span class="offer_brand">' + offer.brand + ' ' + offer.type + '</span>';
-    
     var tagline = '';
 
     switch(offer.type){
