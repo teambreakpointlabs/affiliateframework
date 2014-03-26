489a861aec946d876f7040c350666db502cb5877
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-03-26 14:34:48 +0000

    added offer type to individual offer page

--- public/js/controllers/offers.js
+++ public/js/controllers/offers.js
@@ -30,7 +30,8 @@ angular.module('mean.system').controller('OffersController', ['$scope','Offers',
   $scope.findOne = function(){
     Offers.findByUrlDesc($stateParams.urlDesc).then(function(offer){
       $scope.offer = offer;
-      PageDetailService.setTitle(offer.description + ' | Offercrunch');
+      var capitaliseType = $stateParams.type.charAt(0).toUpperCase() + $stateParams.type.slice(1);
+      PageDetailService.setTitle(offer.description + ' | ' + capitaliseType + ' Offer | Offercrunch');
       PageDetailService.setMetaDescription(offer.description + ' | Offercrunch - All the best online offers in one place. Televisions, laptops, cameras, tablets. Up to 50% off big name brands from major UK retailers.');
       $scope.isLoaded = true;
     });
