5e46f87831f6747b01890b30ca93292daf6887ba
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-03-30 17:04:20 +0100

    about us controller added for title

---
+++ public/js/controllers/about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mean.system').controller('AboutController', ['$scope', 'Global','PageDetailService', function ($scope, Global, PageDetailService) {
+  $scope.global = Global;
+   
+  PageDetailService.setTitle('About Us | Offercrunch ');
+  PageDetailService.setMetaDescription('Offercrunch - All the best online offers in one place. Televisions, laptops, cameras, tablets. Up to 50% off big name brands from major UK retailers.');  
+
+}]);
