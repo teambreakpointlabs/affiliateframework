518906ec167a725aab7f6f3e8bc9b66ed8b74a9c
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-04-17 18:58:50 +0100

    ebuyer tv added

--- public/js/services/data.js
+++ public/js/services/data.js
@@ -1,7 +1,7 @@
 angular.module('mean.system').factory('Data',['$stateParams', function($stateParams){
 	
 	var brands = {
-		television: ['aqualite','bush','cello','lg','panasonic','toshiba','philips','sharp','samsung', 'jvc','sony','hitachi','digihome','logik','sandstrom','jmb','luxor'],
+		television: ['aqualite','bush','cello','lg','panasonic','toshiba','philips','sharp','samsung', 'jvc','sony','hitachi','digihome','logik','sandstrom','jmb','luxor','blaupunkt','foehn & hirsch','furrion','goodmans','hannspree'],
     laptop:['advent','acer','asus','apple','dell','gigabyte','fujitsu','msi','hp','lenovo','samsung','sony','toshiba','packard bell'],
     camera:['fujifilm', 'nikon', 'canon','panasonic','olympus','samsung','vivitar','pentax','polaroid'],
     tablet:['acer','samsung','sony','archos','toshiba','kobo','hp','arnova','prestigio','gemini','asus','google','apple','microsoft','nook','lenovo'],
@@ -16,8 +16,8 @@ angular.module('mean.system').factory('Data',['$stateParams', function($statePar
   }
 
   var retailers = {
-  	television:['argos','currys','littlewoods','direct tvs','selfridges', 'coop electrical','john lewis'],
-  	laptop:['argos','currys','littlewoods','coop electrical','laptops direct'],
+  	television:['argos','currys','littlewoods','ebuyer','selfridges', 'coop electrical','john lewis'],
+  	laptop:['argos','currys','littlewoods','coop electrical'],
   	camera:['argos','currys','littlewoods','jessops'],
   	tablet:['argos','currys','littlewoods'],
     women:{
