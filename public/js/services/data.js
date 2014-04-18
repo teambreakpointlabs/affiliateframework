d160dd41dc20dffe1cc8d3d928cf8809d1467423
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-04-18 21:09:03 +0100

    ebuyer laptops added

--- public/js/services/data.js
+++ public/js/services/data.js
@@ -17,7 +17,7 @@ angular.module('mean.system').factory('Data',['$stateParams', function($statePar
 
   var retailers = {
   	television:['argos','currys','littlewoods','ebuyer','selfridges', 'coop electrical','john lewis'],
-  	laptop:['argos','currys','littlewoods','coop electrical'],
+  	laptop:['argos','currys','littlewoods','coop electrical','ebuyer'],
   	camera:['argos','currys','littlewoods','jessops'],
   	tablet:['argos','currys','littlewoods'],
     women:{
