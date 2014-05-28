528823b2cc146587977f32b69d8737b76ffcacf6
James Haville <jameshaville@Jamess-MacBook-Air.local>
2014-05-28 12:39:02 +0100

    fixed error on server

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ var walk = function(path) {
         var stat = fs.statSync(newPath);
         if (stat.isFile()) {
             if (/(.*)\.(js$|coffee$)/.test(file)) {
-                require(newPath)(app, passport);
+                // require(newPath)(app, passport);
             }
         // We skip the app/routes/middlewares directory as it is meant to be
         // used and shared by routes as further middlewares and is not a 
