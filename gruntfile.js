cee52111febc1496f34bd94cc2ed26c8cb973e23
James Haville <jameshaville@jamess-air.lan>
2014-12-08 23:30:27 +0000

    update

--- gruntfile.js
+++ gruntfile.js
@@ -66,19 +66,20 @@ module.exports = function(grunt) {
             test: {
                 NODE_ENV: 'test'
             }
-        },
-        karma: {
-            unit: {
-                configFile: 'test/karma/karma.conf.js'
-            }
         }
+        // ,
+        // karma: {
+        //     unit: {
+        //         configFile: 'test/karma/karma.conf.js'
+        //     }
+        // }
     });
 
     //Load NPM tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-karma');
+    // grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-env');
@@ -90,5 +91,7 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['jshint', 'concurrent']);
 
     //Test task.
-    grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+    // grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+
+    grunt.registerTask('test', ['env:test', 'mochaTest']);
 };
